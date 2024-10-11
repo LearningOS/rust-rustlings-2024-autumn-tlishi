0d89ccb648f2ec4058c14d59c322144feb79a8a2
tlishi <tlishi2001@gmail.com>
2024-10-11 10:45:15 +0000

    update quiz2

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,11 +3,11 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// 
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    input.chars().filter(|c|!c.is_whitespace()).collect()
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
@@ -18,7 +18,7 @@ fn compose_me(input: &str) -> String {
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    input.replace("balloons","cars").to_string()
+    input.replace("cars","balloons")
 }
 
 #[cfg(test)]
